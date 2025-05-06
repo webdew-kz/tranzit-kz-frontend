3efbca4870d59f76635f22db1bd6579009e4c8c8
webdew-kz <webdew.kz@gmail.com>
2025-05-06 14:05:59 +0500

    Update firebase.ts

--- src/shared/lib/firebase.ts
+++ src/shared/lib/firebase.ts
@@ -1,23 +1,18 @@
 /** @format */
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyC_XVKweXOZURAqGF9KNzVXHdMhxrzlvp0",
     authDomain: "tranzit-1d045.firebaseapp.com",
     projectId: "tranzit-1d045",
-    storageBucket: "tranzit-1d045.firebasestorage.app",
+    storageBucket: "tranzit-1d045.appspot.com",
     messagingSenderId: "1083097584867",
     appId: "1:1083097584867:web:5de8016c44b023f04e21d6",
     measurementId: "G-D5YT33QSJL",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
