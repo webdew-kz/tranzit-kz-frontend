f65b7d6f03b65278742a117a131fc070873191ba
webdew-kz <webdew.kz@gmail.com>
2025-05-07 18:42:57 +0500

    Update index.tsx

--- src/app/(home)/_ui/sections/HeroSection/index.tsx
+++ src/app/(home)/_ui/sections/HeroSection/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function HeroSection() {
 	return (
-		<div className="h-[calc(100vh-48px)] sm:h-[calc(100vh-60px)] bg-[url(/images/bg-home.jpg)] md:bg-[url(/images/bg-home-full.jpg)] bg-no-repeat bg-cover bg-center flex items-center justify-center">
+		<div className="h-[100vh] sm:h-[100vh] bg-[url(/images/bg-home.jpg)] md:bg-[url(/images/bg-home-full.jpg)] bg-no-repeat bg-cover bg-center flex items-center justify-center">
 			<div className="flex flex-col gap-5 items-center justify-center p-5 bg-background w-[calc(100vw-32px)] max-w-full sm:max-w-xs -translate-y-6 rounded-lg opacity-95 border-(--dark-accent) border">
 				<h2 className='text-xl text-center'>Добро пожаловать на платформу <span className='text-(--dark-accent)'>TRANZIT</span></h2>
 				<Button className=' w-full bg-(--dark-accent) hover:bg-(--foreground)' asChild>
