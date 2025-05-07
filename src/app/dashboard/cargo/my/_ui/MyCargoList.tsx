e590d0ada5f24c702b4d3fdc063d69dfdc4f4438
webdew-kz <webdew.kz@gmail.com>
2025-05-07 21:32:41 +0500

    1

--- src/app/dashboard/cargo/my/_ui/MyCargoList.tsx
+++ src/app/dashboard/cargo/my/_ui/MyCargoList.tsx
@@ -158,7 +158,7 @@ export default function MyCargoList() {
 
 	return (
 		<>
-			<Card className='w-full mb-3 lg:mb-5 sticky top-[60px] p-0 rounded-t-none'>
+			<Card className='w-full mb-3 lg:mb-5 sticky top-[120px] md:top-[60px] p-0 rounded-t-none'>
 				<CardContent className=' flex flex-col lg:flex-row gap-3 p-3 lg:p-5 justify-between items-center'>
 					<div className=" grid grid-cols-2 w-full lg:flex ">
 						<Button asChild className={cn(path === '/dashboard/cargo/my' ? 'bg-(--dark-accent)' : 'bg-background text-muted-foreground hover:text-background', 'rounded-r-none')}>
