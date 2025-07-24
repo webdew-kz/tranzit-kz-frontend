80653d94bff5443d873b855e1b6d0c3e8eb56ec6
webdew-kz <webdew.kz@gmail.com>
2025-07-24 12:56:07 +0500

    Update page.tsx

--- src/app/dashboard/payment/page.tsx
+++ src/app/dashboard/payment/page.tsx
@@ -1,10 +1,16 @@
+"use client"
 import { Button } from '@/shared/components/ui/button';
+import { useUserStore } from '@/shared/store/useUserStore';
 import { Box, CirclePlus, Search, Star } from 'lucide-react';
 import Link from 'next/link';
 
 export default function page() {
+
+	const { user } = useUserStore()
+
 	return (
 		<div className=' flex flex-col gap-4 justify-center h-[calc(100vh-160px)]'>
+			<div className="font-semibold text-xl text-center">Ваш баланс: {user?.balance ?? 0}</div>
 			<Button
 				variant={'outline'}
 				className='w-full '
