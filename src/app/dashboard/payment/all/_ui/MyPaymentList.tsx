1152b71b112bad7e67f1bfad9a1c010c5943e1bb
webdew-kz <webdew.kz@gmail.com>
2025-07-28 13:51:45 +0500

    Update MyPaymentList.tsx

--- src/app/dashboard/payment/all/_ui/MyPaymentList.tsx
+++ src/app/dashboard/payment/all/_ui/MyPaymentList.tsx
@@ -108,14 +108,15 @@ export default function MyPaymentList() {
 
 			await deleteAll();
 
+			setPayments([])
+			setTotalAmount(null)
+
 			alert("Все платежи успешно удалены.");
 		} catch (error) {
 			console.error("Ошибка удаления:", error);
 			alert("Произошла ошибка при удалении.");
 		}
 	};
-
-
 
 	if (loading || pending) {
 		return <Loader />
@@ -126,11 +127,13 @@ export default function MyPaymentList() {
 			<Card className='w-full mb-3 lg:mb-5 sticky top-[120px] md:top-[60px] p-0 rounded-t-none'>
 				<CardContent className=' flex flex-col lg:flex-row gap-3 p-3 lg:p-5 justify-between items-center'>
 					<div className=" grid w-full lg:flex ">
-						<Button asChild className='bg-background text-white hover:text-background rounded-r-none'>
-							<span>
-								Всего: {(totalAmount as number)?.toLocaleString('ru-RU')} ₸
-							</span>
-						</Button>
+						{(totalAmount && totalAmount > 0) && (
+							<Button asChild className='bg-background text-white hover:text-background rounded-r-none'>
+								<span>
+									Всего: {(totalAmount as number)?.toLocaleString('ru-RU')} ₸
+								</span>
+							</Button>
+						)}
 
 					</div>
 					<div className="flex items-center gap-4 justify-between w-full lg:justify-end h-[36px]">
