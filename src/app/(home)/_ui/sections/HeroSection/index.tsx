f13bff1307b0bba0262fac2550b37923d7bf5d8b
webdew-kz <webdew.kz@gmail.com>
2025-08-04 17:26:49 +0500

    Update index.tsx

--- src/app/(home)/_ui/sections/HeroSection/index.tsx
+++ src/app/(home)/_ui/sections/HeroSection/index.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function HeroSection() {
 	return (
 		<div className="h-[100vh] sm:h-[100vh] bg-[url(/images/bg-home.jpg)] md:bg-[url(/images/bg-home-full.jpg)] bg-no-repeat bg-cover bg-center flex items-center justify-center">
-			<div className="flex flex-col gap-5 items-center justify-center p-5 bg-background w-[calc(100vw-32px)] max-w-full sm:max-w-xs -translate-y-6 rounded-lg opacity-95 border-(--dark-accent) border">
+			<div className="flex flex-col gap-5 items-center justify-center p-5 bg-background w-[calc(100vw-32px)] max-w-full sm:max-w-xs -translate-y-6 rounded-lg opacity-95 border-(--dark-accent) border" style={{ marginBottom: '60px' }}>
 				<h2 className='text-xl text-center'>Добро пожаловать на платформу <span className='text-(--dark-accent)'>TRANZIT</span></h2>
 				<Button className=' w-full bg-(--dark-accent) hover:bg-(--foreground)' asChild>
 					<Link href='/login'>Вход</Link>
@@ -13,6 +13,11 @@ export default function HeroSection() {
 				<Button className=' w-full border-(--dark-accent) dark:border-(--dark-accent) hover:bg-(--foreground) hover:border-(--foreground) hover:text-(--background) hover:dark:bg-(--foreground) hover:dark:border-(--foreground) hover:dark:text-(--background)' variant="outline" asChild>
 					<Link href='/register'>Регистрация</Link>
 				</Button>
+			</div>
+			<div className="flex flex-col gap-3 text-center">
+				<Link href='/policy' className='text-(--dark-accent) hover:text-(--foreground) border-b border-dashed border-(--dark-accent) hover:border-(--foreground)'>Политика конфиденциальности</Link>
+				<Link href='/aferta' className='text-(--dark-accent) hover:text-(--foreground) border-b border-dashed border-(--dark-accent) hover:border-(--foreground)'>Договор аферты</Link>
+				<Link href='/rekvizit' className='text-(--dark-accent) hover:text-(--foreground) border-b border-dashed border-(--dark-accent) hover:border-(--foreground)'>Реквизиты</Link>
 			</div>
 		</div>
 	)
