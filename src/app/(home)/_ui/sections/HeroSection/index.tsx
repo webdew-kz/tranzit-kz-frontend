e5be4959f6ab0c53366ea1dab812e7d13f654a82
webdew-kz <webdew.kz@gmail.com>
2025-08-05 13:59:10 +0500

    1

--- src/app/(home)/_ui/sections/HeroSection/index.tsx
+++ src/app/(home)/_ui/sections/HeroSection/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function HeroSection() {
 	return (
-		<div className="h-[100vh] sm:h-[100vh] bg-[url(/images/bg-home.jpg)] md:bg-[url(/images/bg-home-full.jpg)] bg-no-repeat bg-cover bg-center flex items-center justify-center">
+		<div className="h-[100vh] sm:h-[100vh] bg-[url(/images/bg-home.jpg)] md:bg-[url(/images/bg-home-full.jpg)] bg-no-repeat bg-cover bg-center flex items-center justify-center flex-col gap-5">
 			<div className="flex flex-col gap-5 items-center justify-center p-5 bg-background w-[calc(100vw-32px)] max-w-full sm:max-w-xs -translate-y-6 rounded-lg opacity-95 border-(--dark-accent) border" style={{ marginBottom: '60px' }}>
 				<h2 className='text-xl text-center'>Добро пожаловать на платформу <span className='text-(--dark-accent)'>TRANZIT</span></h2>
 				<Button className=' w-full bg-(--dark-accent) hover:bg-(--foreground)' asChild>
@@ -15,9 +15,9 @@ export default function HeroSection() {
 				</Button>
 			</div>
 			<div className="flex flex-col gap-3 text-center">
-				<Link href='/policy' className='text-(--dark-accent) hover:text-(--foreground) border-b border-dashed border-(--dark-accent) hover:border-(--foreground)'>Политика конфиденциальности</Link>
-				<Link href='/aferta' className='text-(--dark-accent) hover:text-(--foreground) border-b border-dashed border-(--dark-accent) hover:border-(--foreground)'>Договор аферты</Link>
-				<Link href='/rekvizit' className='text-(--dark-accent) hover:text-(--foreground) border-b border-dashed border-(--dark-accent) hover:border-(--foreground)'>Реквизиты</Link>
+				<Link href='/policy' className='text-white hover:text-(--foreground)'>Политика конфиденциальности</Link>
+				<Link href='/aferta' className='text-white hover:text-(--foreground)'>Договор аферты</Link>
+				<Link href='/rekvizit' className='text-white hover:text-(--foreground)'>Реквизиты</Link>
 			</div>
 		</div>
 	)
