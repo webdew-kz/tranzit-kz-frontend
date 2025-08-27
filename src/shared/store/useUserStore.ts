abd79b4ab1cc4921e917e5da6ca3e973fe9a4985
webdew-kz <webdew.kz@gmail.com>
2025-08-27 14:19:39 +0500

    Update useUserStore.ts

--- src/shared/store/useUserStore.ts
+++ src/shared/store/useUserStore.ts
@@ -7,7 +7,7 @@ import { User } from "../types/user.type";
 interface UserState {
     user: User | null;
     isInitialized: boolean;
-    setUser: (user: User) => void;
+    setUser: (user: User | null) => void;
     clearUser: () => void;
     updateBalance: (newBalance: number) => void;
 }
@@ -16,9 +16,12 @@ export const useUserStore = create<UserState>()(
     persist(
         (set, get) => ({
             user: null,
-            isInitialized: false, // 👈 добавлено
+            isInitialized: false,
+
             setUser: (user) => set({ user, isInitialized: true }),
+
             clearUser: () => set({ user: null, isInitialized: true }),
+
             updateBalance: (newBalance: number) => {
                 const { user } = get();
                 if (user) {
@@ -28,9 +31,12 @@ export const useUserStore = create<UserState>()(
         }),
         {
             name: "user-storage",
-            onRehydrateStorage: () => (state) => {
-                // Когда Zustand инициализирует данные из localStorage
-                state?.setUser(state.user!); // Это вызовет isInitialized: true
+            onRehydrateStorage: () => (state, error) => {
+                // доступ к set можно сделать через state?setUser
+                // или аккуратнее:
+                if (!error) {
+                    state?.setUser(state?.user ?? null);
+                }
             },
         }
     )
