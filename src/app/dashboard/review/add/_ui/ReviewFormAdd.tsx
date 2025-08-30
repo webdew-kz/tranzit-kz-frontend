fa06618b34b2cd42062ca3a83ea4cab7fab21e18
webdew-kz <webdew.kz@gmail.com>
2025-08-30 23:41:35 +0500

    Update ReviewFormAdd.tsx

--- src/app/dashboard/review/add/_ui/ReviewFormAdd.tsx
+++ src/app/dashboard/review/add/_ui/ReviewFormAdd.tsx
@@ -57,7 +57,7 @@ export default function ReviewFormAdd() {
 				return val.replace(/[^\d]/g, '')
 			}
 			return val
-		}, z.string().length(11, { message: 'минимум 11 цифр если номер телефона, 12 цифр если ИИН' })),
+		}, z.string().min(11, { message: 'минимум 11 цифр если номер телефона, 12 цифр если ИИН' })),
 		title: z.string().optional(),
 		description: z.string().optional(),
 		tags: z.array(z.string()).optional(),
