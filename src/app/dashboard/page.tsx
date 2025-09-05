fc145c022dd10fd8732535c9ff00d6a83cbbf043
webdew-kz <webdew.kz@gmail.com>
2025-09-05 18:55:14 +0500

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -34,7 +34,7 @@ export default function DashboardPage() {
 
 	return (
 
-		<div className="h-[calc(100vh-160px)] sm:h-[calc(100vh-60px)] bg-[url(/images/bg-home.jpg)] md:bg-[url(/images/bg-home-full.jpg)] bg-no-repeat bg-cover bg-center flex items-start pt-5 justify-center">
+		<div className="h-[calc(100vh-160px)] sm:h-[calc(100vh-60px)] flex items-start pt-5 justify-center">
 			<div className="flex flex-col gap-5 items-center justify-center p-5 bg-background w-[calc(100vw-32px)] max-w-full sm:max-w-xs -translate-y-6 rounded-lg border-(--dark-accent) border opacity-70">
 				<h2 className='text-xl text-center'>Здравствуйте, {user?.name} {user?.surname}</h2>
 				{user.role === 'ADMIN' ? <span className='text-(--dark-accent)'>Статус: Администратор</span> : null}
