fb61f3b4e965520a67f628251af719d4a567b622
webdew-kz <webdew.kz@gmail.com>
2025-09-05 18:59:55 +0500

    Update page.tsx

--- src/app/dashboard/cargo/page.tsx
+++ src/app/dashboard/cargo/page.tsx
@@ -6,8 +6,8 @@ export default function page() {
 	return (
 		<div className=' flex flex-col gap-4 justify-center h-[calc(100vh-160px)] '>
 			<Button
-				variant={'outline'}
-				className='w-full sm:hidden'
+				variant='default'
+				className='w-full sm:hidden bg-accent'
 				asChild
 			>
 
