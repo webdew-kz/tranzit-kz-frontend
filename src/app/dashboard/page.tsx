02eede1bc178e489888779f692c3fe82866fded8
webdew-kz <webdew.kz@gmail.com>
2025-09-07 19:08:06 +0500

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,8 @@ import { useUserStore } from '@/shared/store/useUserStore'
 import React from 'react'
 import Loader from '@/shared/components/widgets/Loader'
 import FormName from './_ui/FormName'
+import { Button } from '@/shared/components/ui/button'
+import Link from 'next/link'
 
 export default function DashboardPage() {
 
@@ -24,14 +26,6 @@ export default function DashboardPage() {
 		)
 	}
 
-	// if (!user?.isRegistered && user?.role === 'USER') {
-	// 	return (
-	// 		<div className=' fixed left-0 bottom-0 right-0 top-[60px] bg-background z-100 flex justify-center items-center'>
-	// 			<PayRegister />
-	// 		</div>
-	// 	)
-	// }
-
 	return (
 
 		<div className="h-[calc(100vh-160px)] sm:h-[calc(100vh-60px)] flex items-start pt-5 justify-center">
@@ -39,6 +33,17 @@ export default function DashboardPage() {
 				<h2 className='text-xl text-center'>Здравствуйте, {user?.name} {user?.surname}</h2>
 				{user.role === 'ADMIN' ? <span className='text-(--dark-accent)'>Статус: Администратор</span> : null}
 			</div>
+			{!user?.isRegistered && (
+				<div className="flex flex-col justify-center w-full gap-3 md:gap-5 items-center">
+					<div className="text-center">Для полного доступа к приложению требуется абонентская плата 1000 тенге за 1 месяц. <br /> Абонентская плата предоставляет право размещать, копировать, редактировать и удалять собственные объявления, а также открывает доступ к контактной информации в чужих объявлениях.</div>
+					<Button
+						className=' bg-(--dark-accent)'
+						asChild
+					>
+						<Link href='/dashboard/payment/pay-register'>Перейти к оплате</Link>
+					</Button>
+				</div>
+			)}
 		</div>
 	)
 }
