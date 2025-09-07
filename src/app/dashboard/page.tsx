128a735cacffb3e8f2ec72ac177781f8219111cd
webdew-kz <webdew.kz@gmail.com>
2025-09-07 20:36:12 +0500

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -29,21 +29,23 @@ export default function DashboardPage() {
 	return (
 
 		<div className="h-[calc(100vh-160px)] sm:h-[calc(100vh-60px)] flex flex-col gap-3 md:gap-5 items-start pt-5 justify-center">
-			<div className="flex flex-col gap-5 items-center justify-center p-5 bg-background w-[calc(100vw-32px)] max-w-full sm:max-w-xs -translate-y-6 rounded-lg border-(--dark-accent) border opacity-70">
+			<div className="flex flex-col gap-3 md:gap-5  items-center justify-center p-5 bg-background w-[calc(100vw-32px)] max-w-full sm:max-w-xs -translate-y-6 rounded-lg border-(--dark-accent) border opacity-70">
 				<h2 className='text-xl text-center'>Здравствуйте, {user?.name} {user?.surname}</h2>
 				{user.role === 'ADMIN' ? <span className='text-(--dark-accent)'>Статус: Администратор</span> : null}
+
+
+				{!user?.isRegistered && (
+					<div className="flex flex-col justify-center w-full gap-3 md:gap-5 items-center">
+						<div className="text-center">Для полного доступа к приложению требуется абонентская плата 1000 тенге за 1 месяц. <br /> Абонентская плата предоставляет право размещать, копировать, редактировать и удалять собственные объявления, а также открывает доступ к контактной информации в чужих объявлениях.</div>
+						<Button
+							className=' bg-(--dark-accent)'
+							asChild
+						>
+							<Link href='/dashboard/payment/pay-register'>Перейти к оплате</Link>
+						</Button>
+					</div>
+				)}
 			</div>
-			{!user?.isRegistered && (
-				<div className="flex flex-col justify-center w-full gap-3 md:gap-5 items-center">
-					<div className="text-center">Для полного доступа к приложению требуется абонентская плата 1000 тенге за 1 месяц. <br /> Абонентская плата предоставляет право размещать, копировать, редактировать и удалять собственные объявления, а также открывает доступ к контактной информации в чужих объявлениях.</div>
-					<Button
-						className=' bg-(--dark-accent)'
-						asChild
-					>
-						<Link href='/dashboard/payment/pay-register'>Перейти к оплате</Link>
-					</Button>
-				</div>
-			)}
 		</div>
 	)
 }
