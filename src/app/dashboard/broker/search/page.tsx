e84a652af7ab3341f932f628eff30039317f6c3c
webdew-kz <webdew.kz@gmail.com>
2025-10-09 13:24:07 +0500

    Update page.tsx

--- src/app/dashboard/broker/search/page.tsx
+++ src/app/dashboard/broker/search/page.tsx
@@ -1,14 +1,11 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 import BrokerSearchList from './_ui/BrokerSearchList';
 import BrokerFormSearch from './_ui/BrokerFormSearch';
 
 export default function page() {
 	return (
 		<div className='grid gap-5'>
-			<BrokerFormSearch />
-			{/* <BrokerSearchList /> */}
+			{/* <BrokerFormSearch /> */}
+			<BrokerSearchList />
 		</div>
 	)
 }
